5b7b39106ce22778acd7b4b40f44c513f441319c
JOSE ARTURO SANCHEZ SOTO <P027140@PE.igrupobbva>
2019-04-23 11:06:46 -0500

    .

--- persona-dm-element.js
+++ persona-dm-element.js
@@ -26,10 +26,31 @@ class PersonaDmElement extends PolymerElement {
       </iron-ajax>
 
       <iron-ajax
+          id = "listPersonas"
+          url="{{baseUrlApi}}/personas"
+          handle-as="json" >
+      </iron-ajax>
+
+      <iron-ajax
           id = "create"
           url="{{baseUrlApi}}/persona"
           method="post"
           content-type = "application/json"
+          handle-as="json" >
+      </iron-ajax>
+
+      <iron-ajax
+          id = "update"
+          url="{{baseUrlApi}}/persona/{{idPersona}}"
+          method="put"
+          content-type = "application/json"
+          handle-as="json" >
+      </iron-ajax>
+
+      <iron-ajax
+          id = "delete"
+          url="{{baseUrlApi}}/persona/{{idPersona}}"
+          method="delete"
           handle-as="json" >
       </iron-ajax>
 
@@ -41,7 +62,8 @@ class PersonaDmElement extends PolymerElement {
       baseUrlApi: {
         type: String,
         value: 'http://138.68.28.167:3000'
-      }
+      },
+      idPersona: String
     };
   }
 
@@ -52,6 +74,21 @@ class PersonaDmElement extends PolymerElement {
   create(body, onSuccess, onError) {
     this.$.create.body = JSON.stringify(body);
     this.$.create.generateRequest().completes.then(onSuccess || ((xhr)=>console.log(xhr)) ).catch(onError || ((error)=>console.log(error)));
+  }
+
+  update(id, body, onSuccess, onError) {
+    this.idPersona = id;
+    this.$.update.body = JSON.stringify(body);
+    this.$.update.generateRequest().completes.then(onSuccess || ((xhr)=>console.log(xhr)) ).catch(onError || ((error)=>console.log(error)));
+  }
+
+  find(onSuccess, onError) {
+    this.$.listPersonas.generateRequest().completes.then(onSuccess || ((xhr)=>console.log(xhr)) ).catch(onError || ((error)=>console.log(error)));
+  }
+
+  delete(id, onSuccess, onError) {
+    this.idPersona = id;
+    this.$.delete.generateRequest().completes.then(onSuccess || ((xhr)=>console.log(xhr)) ).catch(onError || ((error)=>console.log(error)));
   }
 
 }
